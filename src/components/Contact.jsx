ddac1b90178a61dc247d1e5eff54af28ee190300
Malo Pivert <pivertmalo@gmail.com>
2020-10-14 12:33:57 +0200

    map users and called contact in contactlist

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,8 @@ import "./Contact.css";
 
 
 
-function Contact({name,avatar,Online}) {
+
+function Contact({name,avatar,online}) {
   return (
     <div className="Contact">
       <img
@@ -17,13 +18,38 @@ function Contact({name,avatar,Online}) {
           <h4>{name}</h4>
         </div>
         <div className="status">
-          <div className={Online ? "status-online" : "status-offline"}></div>
-          <p className="status-text">{Online ? "Online" : "Offline"}</p>
+          <div className={online ? "status-online" : "status-offline"}></div>
+          <p className="status-text">{online ? "Online" : "Offline"}</p>
         </div>
       </div>
     </div>
   );
 }
+
+
+
+
+
+/* function Contact({name,avatar,online}) {
+  return (
+    <div className="Contact">
+      <img
+        className="avatar"
+        src={avatar}
+        alt={name}
+      />
+      <div>
+        <div className="name">
+          <h4>{name}</h4>
+        </div>
+        <div className="status">
+          <div className={online ? "status-online" : "status-offline"}></div>
+          <p className="status-text">{online ? "Online" : "Offline"}</p>
+        </div>
+      </div>
+    </div>
+  );
+} */
 
 Contact.propTypes = {
   name : PropTypes.string.isRequired,
