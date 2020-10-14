06281a074cb22efba88c39267f848a94ccb340ab
Malo Pivert <pivertmalo@gmail.com>
2020-10-14 12:36:27 +0200

    added app.js because git was not

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import Contact from './components/Contact';
+/* import Contact from './components/Contact'; */
+import ContactList from './components/ContactList';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Contact name="Youtube Tutorial" avatar="https://randomuser.me/api/portraits/men/69.jpg" Online />
+{/*       <Contact name="Youtube Tutorial" avatar="https://randomuser.me/api/portraits/men/69.jpg" online />
       <Contact name="Middle Age" avatar="https://randomuser.me/api/portraits/women/10.jpg" />
-      <Contact name="Barack Obama" avatar="https://images-na.ssl-images-amazon.com/images/I/511112vA1iL._AC_UX466_.jpg" Online/>
+      <Contact name="Barack Obama" avatar="https://images-na.ssl-images-amazon.com/images/I/511112vA1iL._AC_UX466_.jpg" online/> */}
+      <ContactList/>
+      
     </div>
   );
 }
