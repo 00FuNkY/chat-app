d7d8453c334ba466bc4e2f11a9130689b1d22105
Malo Pivert <pivertmalo@gmail.com>
2020-10-20 19:23:03 +0200

    the status can change by cliking on it

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,34 +1,43 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./Contact.css";
+import { Component } from "react";
 
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: this.props.online,
+    };
+  }
 
-
-
-function Contact({name,avatar,online}) {
-  return (
-    <div className="Contact">
-      <img
-        className="avatar"
-        src={avatar}
-        alt={name}
-      />
-      <div>
-        <div className="name">
-          <h4>{name}</h4>
-        </div>
-        <div className="status">
-          <div className={online ? "status-online" : "status-offline"}></div>
-          <p className="status-text">{online ? "Online" : "Offline"}</p>
+  render() {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+        <div>
+          <div className="name">
+            <h4>{this.props.name}</h4>
+          </div>
+          <div className="status">
+            <div
+              className={this.state.online ? "status-online" : "status-offline"}
+            ></div>
+            <p
+              onClick={(event) => {
+                const newStatus = !this.state.online;
+                this.setState({online : newStatus})
+              }}
+              className="status-text"
+            >
+              {this.state.online ? "Online" : "Offline"}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-
-
-
 
 /* function Contact({name,avatar,online}) {
   return (
@@ -52,9 +61,9 @@ function Contact({name,avatar,online}) {
 } */
 
 Contact.propTypes = {
-  name : PropTypes.string.isRequired,
-  avatar : PropTypes.string.isRequired,
-  Online : PropTypes.bool,
-}
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  Online: PropTypes.bool,
+};
 
 export default Contact;
